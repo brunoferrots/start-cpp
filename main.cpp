ad588acde9fa264a7bf050d1fe09cfdc8c7e96fb
brunoferrots <brunoferrots@outlook.com>
2023-10-31 20:33:13 -0300

    test uninitialized variable

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // * Algorithm for convert Celcius for Fahrenheit
 
-int main(int numOfArgs, char* pszArgs[]) {
+/* int main(int numOfArgs, char* pszArgs[]) {
   int celsius = 0;
   cout << "Enter temperature in Celcius\n";
   cin >> celsius;
@@ -15,5 +15,14 @@ int main(int numOfArgs, char* pszArgs[]) {
 
   cout << "Fahrenheit value is: " << fahrenheit << endl;
   system("PAUSE");
+  return 0;
+} */
+
+int main() {
+  int n1, n2 = 0;
+  n2 = n1 + 1;
+
+  cout << "n1 = " << n1 << endl;
+  cout << "n2 = " << n2 << endl;
   return 0;
 }
